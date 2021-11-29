a49c753f40c58aedae0d359e67fe2d9f914f0e67
fthbby <fthbby@gmail.com>
2021-11-28 22:01:04 -0700

    chose a different API

--- script.js
+++ script.js
@@ -5,13 +5,66 @@ console.log('hello')
 
 // http://acnhapi.com/v1/villagers/{villagerID}
 
-$.ajax({
-    url: 'https://acnhapi.com/v1/villagers/',
-    success: function(name){
-        console.log(name);
-    },
-    error: function(error){
-        console.log('bad requests: ',  error)
-    }
-});
+// $.ajax({
+//     url: 'https://api.tvmaze.com/singlesearch/shows?q=girls',
+//     success: function(data){
+//         console.log(data);
+//         console.log(data.language)
+//         console.log(data.genres)
+//         // console.log(data.height);
+
+//     },
+//     error: function(error){
+//         console.log('bad requests: ',  error)
+//     }
+// });
+
+// $('form').on('submit',(event)=> {
+//         event.preventDefault();
+//         const userInput = $('input[type='text']').val();
+//         $.ajax({
+//             url: `https://acnhapi.com/v1a/villagers/${userInput}`;
+//     }).then(
+//         (data) => {
+//         $('#name').html(data.name['name-USen'])
+//         $('#birthday').html(data.birthday);
+//         $('#species').html(data.species);
+//         },
+//         (error) =>{
+//             console.log('bad, no good: ', error)
+//         } 
+//     );
+// });
+// const url = `https://acnhapi.com/v1a/villagers/${userInput}`
+
+// async function getVillagerData(e) {
+//     e.preventDefault();
+//     const userInput = $('input[type="text"]').val();
+//     const url = `https://acnhapi.com/v1a/villagers/${userInput}`;
+
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     $('#name').html(data.name['name-USen'])
+//     $('#birthday').html(data.birthday);
+//     $('#species').html(data.species);
+// }
+
+// $('form').on('submit', getVillagerData);
+
+
+async function getShowData(e) {
+    e.preventDefault();
+    const userInput = $('input[type="text"]').val();
+    const url = `https://api.tvmaze.com/singlesearch/shows?q=${userInput}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    $('#name').html(data.name);
+    $('#language').html(data.language);
+    $('#genres').html(data.genres);
+}
+
+$('form').on('submit', getShowData);
 
